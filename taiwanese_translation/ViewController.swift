d831af14ee9b9b636d52acc7486321770723f4ad
OS Mac <thh101u@MactekiMac.local>
2018-07-28 20:50:02 +0800

    Initial Commit

---
+++ taiwanese_translation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taiwanese_translation
+//
+//  Created by Mac OS on 2018/7/28.
+//  Copyright © 2018年 taiwanspeech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
