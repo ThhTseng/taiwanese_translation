a0d899dbb2ce5c3ca01a56c6bb7460f714864bce
OS Mac <thh101u@MactekiMac.local>
2018-09-07 14:12:38 +0800

    9/7 post version

--- taiwanese_translation/ViewController.swift
+++ taiwanese_translation/ViewController.swift
@@ -7,19 +7,288 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var play_btn_ref: UIButton!
+    @IBOutlet weak var record_btn_ref: UIButton!
+    @IBOutlet weak var recordingTimeLabel: UILabel!
+    @IBOutlet weak var recognized_text: UITextField!
+    @IBOutlet weak var upload_btn_ref: UIButton!
+    
+    
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer!
+    var meterTimer:Timer!
+    var isAudioRecordingGranted:Bool!
+    var isRecording = false
+    var isPlaying = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        check_record_permission()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func check_record_permission(){
+        switch AVAudioSession.sharedInstance().recordPermission() {
+        case AVAudioSessionRecordPermission.granted:
+            isAudioRecordingGranted = true
+            break
+        case AVAudioSessionRecordPermission.denied:
+            isAudioRecordingGranted = false
+            break
+        case AVAudioSessionRecordPermission.undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission({ (allowed) in
+                if allowed {
+                    self.isAudioRecordingGranted = true
+                } else {
+                    self.isAudioRecordingGranted = false
+                }
+            })
+            break
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL
+    {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    func getFileUrl() -> URL
+    {
+        let filename = "myRecording.m4a"
+        let filePath = getDocumentsDirectory().appendingPathComponent(filename)
+        return filePath
+    }
+    
+    func setup_recorder()
+    {
+        if isAudioRecordingGranted
+        {
+            let session = AVAudioSession.sharedInstance()
+            do
+            {
+                try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
+                try session.setActive(true)
+                let settings = [
+                    AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                    AVSampleRateKey: 44100,
+                    AVNumberOfChannelsKey: 1,
+                    AVEncoderAudioQualityKey:AVAudioQuality.high.rawValue
+                ]
+                audioRecorder = try AVAudioRecorder(url: getFileUrl(), settings: settings)
+                audioRecorder.delegate = self
+                audioRecorder.isMeteringEnabled = true
+                audioRecorder.prepareToRecord()
+            }
+            catch let error {
+                display_alert(msg_title: "Error", msg_desc: error.localizedDescription, action_title: "OK")
+            }
+        }
+        else
+        {
+            display_alert(msg_title: "Error", msg_desc: "Don't have access to use your microphone.", action_title: "OK")
+        }
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func start_recording(_ sender: UIButton) {
+        if(isRecording)
+        {
+            finishAudioRecording(success: true)
+            record_btn_ref.setTitle("Record", for: .normal)
+            play_btn_ref.isEnabled = true
+            isRecording = false
+        }
+        else
+        {
+            setup_recorder()
+            
+            audioRecorder.record()
+            meterTimer = Timer.scheduledTimer(timeInterval: 0.1, target:self, selector:#selector(self.updateAudioMeter(timer:)), userInfo:nil, repeats:true)
+            record_btn_ref.setTitle("Stop", for: .normal)
+            play_btn_ref.isEnabled = false
+            isRecording = true
+        }
+    }
+    @objc func updateAudioMeter(timer: Timer)
+    {
+        if audioRecorder.isRecording
+        {
+            let hr = Int((audioRecorder.currentTime / 60) / 60)
+            let min = Int(audioRecorder.currentTime / 60)
+            let sec = Int(audioRecorder.currentTime.truncatingRemainder(dividingBy: 60))
+            let totalTimeString = String(format: "%02d:%02d:%02d", hr, min, sec)
+            recordingTimeLabel.text = totalTimeString
+            audioRecorder.updateMeters()
+        }
+    }
+    func finishAudioRecording(success: Bool)
+    {
+        if success
+        {
+            audioRecorder.stop()
+            audioRecorder = nil
+            meterTimer.invalidate()
+            print("recorded successfully.")
+        }
+        else
+        {
+            display_alert(msg_title: "Error", msg_desc: "Recording failed.", action_title: "OK")
+        }
+    }
+    //Play the recording
+    func prepare_play()
+    {
+        do
+        {
+            audioPlayer = try AVAudioPlayer(contentsOf: getFileUrl())
+            audioPlayer.delegate = self
+            audioPlayer.prepareToPlay()
+        }
+        catch{
+            print("Error")
+        }
+    }
+    
+    @IBAction func play_recording(_ sender: Any) {
+        if(isPlaying)
+        {
+            audioPlayer.stop()
+            record_btn_ref.isEnabled = true
+            play_btn_ref.setTitle("Play", for: .normal)
+            isPlaying = false
+        }
+        else
+        {
+            if FileManager.default.fileExists(atPath: getFileUrl().path)
+            {
+                record_btn_ref.isEnabled = false
+                play_btn_ref.setTitle("pause", for: .normal)
+                prepare_play()
+                audioPlayer.play()
+                isPlaying = true
+            }
+            else
+            {
+                display_alert(msg_title: "Error", msg_desc: "Audio file is missing.", action_title: "OK")
+            }
+        }
+    }
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool)
+    {
+        if !flag
+        {
+            finishAudioRecording(success: false)
+        }
+        play_btn_ref.isEnabled = true
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool)
+    {
+        record_btn_ref.isEnabled = true
+    }
+    
+    func display_alert(msg_title : String , msg_desc : String ,action_title : String)
+    {
+        let ac = UIAlertController(title: msg_title, message: msg_desc, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: action_title, style: .default)
+        {
+            (result : UIAlertAction) -> Void in
+            _ = self.navigationController?.popViewController(animated: true)
+        })
+        present(ac, animated: true)
+    }
+    
+    @IBAction func uploadfile(_ sender: Any) {
+        if FileManager.default.fileExists(atPath: getFileUrl().path)
+        {
+            /*
+            let client=TCPClient(address:"140.116.245.151" ,port:2803)
+            switch client.connect(timeout:10){
+            case .success:
+                let token:String="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJpc3MiOiJKV1QiLCJhdWQiOiJ3bW1rcy5jc2llLmVkdS50dyIsInNlcnZpY2UiOiI4ODgiLCJ1c2VybmFtZSI6Imxvcm5lIiwic2NvcGVzIjoiMSIsInN1YiI6IjIiLCJpYXQiOjE1MzMwMzcxMDEsImV4cCI6MTU5NjEwOTEwMSwibmJmIjoxNTMzMDM3MTAxfQ.ROgcmbmXS2KYmsKkdZCI3UI56iLdLneXlKaj4qJujCYSqlpPKOHVN9J0eHR_OrYg-sMQEf06XiO52KqO-makYBJ5bRe134M8UcU06XrkC6v0KUMtJHtkBTpCAIIa14__ifZjlFFA4EGcX4DY0r8XFEkuEpEu3Gb_88_fJmUquXw"
+                
+                //let fileURL = getFileUrl()
+                
+                let data:String = token+String("@@@S07     Atestdata")
+                var length:String=String(data.count)
+                length=length+data
+                print(length)
+                switch client.send(string:length){
+                case .success:
+                    guard let data=client.read(1024*10)else {return}
+                    if let result = String(bytes:data,encoding:.utf8){
+                        self.recognized_text.text="\(result)"
+                        
+                    }
+                case .failure(let error):
+                    print(error)
+                    
+                }                
+            case .failure(let error):
+                print(error)
+            }
+            */
+            let token:String="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJpc3MiOiJKV1QiLCJhdWQiOiJ3bW1rcy5jc2llLmVkdS50dyIsInNlcnZpY2UiOiI4ODgiLCJ1c2VybmFtZSI6Imxvcm5lIiwic2NvcGVzIjoiMSIsInN1YiI6IjIiLCJpYXQiOjE1MzMwMzcxMDEsImV4cCI6MTU5NjEwOTEwMSwibmJmIjoxNTMzMDM3MTAxfQ.ROgcmbmXS2KYmsKkdZCI3UI56iLdLneXlKaj4qJujCYSqlpPKOHVN9J0eHR_OrYg-sMQEf06XiO52KqO-makYBJ5bRe134M8UcU06XrkC6v0KUMtJHtkBTpCAIIa14__ifZjlFFA4EGcX4DY0r8XFEkuEpEu3Gb_88_fJmUquXw"
+            
+            let fileURL = getFileUrl()
+            
+            
+            var data:String = token+String("@@@S07     A")
 
+            do{
+                let audioData = try Data(contentsOf: fileURL)
+                
+                let encodedString = audioData.base64EncodedString()
+                
+                data+=encodedString
+                print(data)
+                let url = URL(string: "http://140.116.245.217:2803")!
+                var request = URLRequest(url: url)
+                request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+                request.httpMethod = "POST"
+                
+                request.httpBody = data.data(using: .utf8)
+                let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                    guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                        print("error=\(String(describing: error))")
+                        return
+                    }
+                    
+                    if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                        print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                        print("response = \(String(describing: response))")
+                    }
+                    
+                    let responseString = String(data: data, encoding: .utf8)
+                    //self.recognized_text.text="\(String(describing: responseString))"
+                    print("responseString = \(String(describing: responseString))")
+                }
+                task.resume()
 
+            }catch{
+                print("error")
+            }
+            
+            
+        }
+        else
+        {
+            display_alert(msg_title: "Error", msg_desc: "Audio file has not been recorded yet!", action_title: "OK")
+        }
+    }
+    
+    
 }
 
